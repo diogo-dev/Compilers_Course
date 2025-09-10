4aee119924f756d50cc6a25db687fe22e0319c5d
Diogo Felipe <diogofelipe.dev@gmail.com>
2025-09-10 16:59:02 -0300

    Building the lexer: 2 (handling invalid tokens)

--- lexer.py
+++ lexer.py
@@ -6,6 +6,25 @@ import ply.lex as lex
 # 3 - lineno (which is the current line number)
 # 4 - lexpos (position of the token relative to the beginning of the input text)
 
+# Important observation about lexer attributes
+'''
+All internal attributes of the lexer (with the exception of lineno) have names that are prefixed by lex 
+(e.g., lexdata,``lexpos``, etc.). Thus, it is perfectly safe to store attributes in the lexer 
+that do not have names starting with that prefix or a name that conflicts with one of the predefined methods 
+(e.g., input(), token(), etc.).
+
+We've used a internal lexer attribute called 'errors' to store lexical errors found during the analysis.
+We initialize it as an empty list when we create the lexer object and we append tuples with information
+about the errors found (line number, character and position in the input text).
+'''
+
+# Another important observation about token rules for reserved words
+'''
+Note: You should avoid writing individual rules for reserved words
+so these rules will be triggered for identifiers that include those words as a prefix, like "forget"
+t_FOR = r'for' 
+'''
+
 # Dictionaty of reserved words for RASCAL
 reserved = {
     "program": "PROGRAM",
@@ -26,10 +45,13 @@ reserved = {
     "and": "AND",
     "or": "OR",
     "not": "NOT",
-    "div": "DIV",
-    "integer": "TYPE",
-    "boolean": "TYPE"
+    "div": "DIV"
 }
+
+# If is necessaty to pass the invalid tokens to the parser, we should add them to the tokens list 
+# invalid_tokens = [
+#     'INVALID_NUMID'
+# ]
 
 # The list token names
 tokens = [
@@ -52,7 +74,7 @@ tokens = [
     'LOWER_E',
     'ASSIGN',
     'COLON'
-] + list(reserved.values())
+] + list(reserved.values()) 
 
 def MyLexer():
     # Regular expression rules for simple tokens
@@ -75,12 +97,24 @@ def MyLexer():
     # Ignore white spaces and tabs
     t_ignore = ' \t'
 
-    # Note: You should avoid writing individual rules for reserved words
-    # so these rules will be triggered for identifiers that include those words as a prefix, like "forget"
-    # t_FOR = r'for' 
-
-    # A regular expression rule with some action code 
-    # in this case the argument 't' is always an instance of LexToken
+    # Invalid handling rule for numbers followed by letters (e.g., 123abc)
+    def t_INVALID_NUMID(t):
+        r'\d+[a-zA-Z_]+'
+        print(f"Erro léxico: Formação incorreta de identificador '{t.value}'")
+        ch = t.value
+        lineno = t.lexer.lineno # linha do erro
+        pos = t.lexer.lexpos  # posição absoluta do erro
+        # armazena uma tupla com informações do erro
+        t.lexer.errors.append((lineno, ch, pos))
+    
+    # Invalid handling rule for floating point numbers (e.g., 12.34 or .56)
+    def t_INVALID_FLOAT_NUMBER(t):
+        r'\d+\.\d*|\.\d+'
+        print(f"Erro léxico: Número em ponto flutuante não permitido '{t.value}'")
+        ch = t.value
+        lineno = t.lexer.lineno 
+        pos = t.lexer.lexpos  
+        t.lexer.errors.append((lineno, ch, pos))
 
     # Number rule
     def t_NUMBER(t):
@@ -89,9 +123,9 @@ def MyLexer():
         return t
 
     # Token TYPE rule (different approach than the one used in reserved words)
-    #   def t_TYPE(t):
-    #       r'integer|boolean'
-    #       return t
+    def t_TYPE(t):
+        r'integer|boolean'
+        return t
 
     # Identifiers rule
     def t_ID(t):
@@ -104,18 +138,19 @@ def MyLexer():
         r'\n+'
         t.lexer.lineno += len(t.value)
 
-    # Error handling rule
+    # Error handling rule for illegal characters (e.g., @, $, %, ^, &)
     def t_error(t):
         print("Illegal character:", t.value[0])
         ch = t.value[0]
-        lineno = t.lexer.lineno # linha do erro
-        pos = t.lexer.lexpos  # posição absoluta do erro
-        # armazena uma tupla com informações do erro
+        lineno = t.lexer.lineno 
+        pos = t.lexer.lexpos  
         t.lexer.errors.append((lineno, ch, pos))
         t.lexer.skip(1)
   
+    # Build the lexer
     lexer = lex.lex()
     lexer.errors = [] 
+    
     return lexer
 
 def main():
@@ -130,7 +165,7 @@ def main():
 
     print("Tokens:")
     for tok in lexer:
-        print(tok)
+        print(f"({tok.type}, {repr(tok.value)})")
     
     if lexer.errors:
         print(f"\n{len(lexer.errors)} erro(s) léxico(s) encontrado(s):")
